237056072a003cfe25fcee0b7dd9c1f588d57fbb
RAJENDRA <rajendra7427@gmail.com>
2023-05-01 22:56:45 +0530

    Initial Commit

---
+++ functions1.py
@@ -0,0 +1,16 @@
+FILEPATH = 'todos.txt'
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return list of to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+        return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write to-do items in a text file."""
+    with open(filepath, 'w') as file1_local:
+        file1_local.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("Hello")
